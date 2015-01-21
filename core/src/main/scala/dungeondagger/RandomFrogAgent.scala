d9afa264a51a828c99b52854366aa29029a37bd3
Evgeniy Tsvigun <utgarda@gmail.com>
2015-01-22 02:37:24 +0300

    random frogs move choice fixed

--- core/src/main/scala/dungeondagger/RandomFrogAgent.scala
+++ core/src/main/scala/dungeondagger/RandomFrogAgent.scala
@@ -3,7 +3,7 @@ package dungeondagger
 class RandomFrogAgent(world: World) extends Agent(AgentKind.Frog){
   override def act: Option[Action] = {
     if(world.rand.nextBoolean()){
-      Some(Move(this, world.rand.nextInt(4).toByte))
+      Some(Move(this, world.rand.nextInt(6).toByte))
     } else if(world.rand.nextInt(100) == 0){
       Some(Die)
     } else None
